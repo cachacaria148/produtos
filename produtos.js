c20cfb3e32c1c3ed7e388783022d484cac004734
cachacaria148 <104029974+cachacaria148@users.noreply.github.com>
2022-04-19 16:03:03 -0300

    Create produtos.js

---
+++ produtos.js
@@ -0,0 +1,525 @@
+var produtos_detalhes = [
+        
+         {
+            produto: 90323263,
+            alcool: "42%",
+            tempo: "02 anos",
+            madeira: "Carvalho",
+            cidade: "Felixlândia",
+            estado: "MG"
+        },
+         
+         {
+            produto: 110262742,
+            alcool: "42%",
+            tempo: "01 ano",
+            madeira: "Carvalho Francês",
+            cidade: "São Roque do Canaã",
+            estado: "ES"
+        },
+         
+         {
+            produto: 110261413,
+            alcool: "40%",
+            tempo: "03 anos",
+            madeira: "Carvalho Francês",
+            cidade: "São Roque do Canaã",
+            estado: "ES"
+        },
+         
+         {
+            produto: 110262281,
+            alcool: "46%",
+            tempo: "01 ano",
+            madeira: "Inox",
+            cidade: "São Roque do Canaã",
+            estado: "ES"
+        },
+         
+         {
+            produto: 110263868,
+            alcool: "42%",
+            tempo: "1 ano Carvalho Francês 1 ano Amburana",
+            madeira: "Carvalho Francês e Amburana",
+            cidade: "São Roque do Canaã",
+            estado: "ES"
+        },
+         
+         {
+            produto: 144501456,
+            alcool: "42%",
+            tempo: "01 ano",
+            madeira: "Amburana",
+            cidade: "Papagaios",
+            estado: "MG"
+        },
+         
+         {
+            produto: 84287027,
+            alcool: "42%",
+            tempo: "01 ano",
+            madeira: "Amburana",
+            cidade: "Papagaios",
+            estado: "MG"
+        },
+         
+         {
+            produto: 145254414,
+            alcool: "40%",
+            tempo: "-",
+            madeira: "Inox",
+            cidade: "Jandaia do Sul",
+            estado: "PR"
+        },
+         
+         {
+            produto: 145255201,
+            alcool: "40%",
+            tempo: "08 anos",
+            madeira: "Carvalho Americano e Francês",
+            cidade: "Jandaia do Sul",
+            estado: "PR"
+        },
+         
+         {
+            produto: 145255728,
+            alcool: "40%",
+            tempo: "04 anos",
+            madeira: "Carvalho Americano e Europeu",
+            cidade: "Jandaia do Sul",
+            estado: "PR"
+        },
+         
+         {
+            produto: 145256563,
+            alcool: "40%",
+            tempo: "02 anos",
+            madeira: "Amburana",
+            cidade: "Jandaia do Sul",
+            estado: "PR"
+        },
+         
+         {
+            produto: 145253703,
+            alcool: "40%",
+            tempo: "08 anos",
+            madeira: "Carvalho Francês, Americano, Amburana e Castanheira",
+            cidade: "Jandaia do Sul",
+            estado: "PR"
+        },
+         
+         {
+            produto: 146106108,
+            alcool: "42%",
+            tempo: "01 ano e 06 meses",
+            madeira: "Carvalho",
+            cidade: "Lençóis Paulista",
+            estado: "SP"
+        },
+         
+         
+         {
+            produto: 146105110,
+            alcool: "42%",
+            tempo: "03 anos",
+            madeira: "Carvalho",
+            cidade: "Lençóis Paulista",
+            estado: "SP"
+        },
+         
+         
+         {
+            produto: 146106681,
+            alcool: "42%",
+            tempo: "06 meses",
+            madeira: "Amendoim",
+            cidade: "Lençóis Paulista",
+            estado: "SP"
+        },
+         
+         {
+            produto: 147155221,
+            alcool: "40%",
+            tempo: "-",
+            madeira: "Inox",
+            cidade: "Indaiabira",
+            estado: "MG"
+        },
+         
+         {
+            produto: 147155013,
+            alcool: "40%",
+            tempo: "04 anos",
+            madeira: "Carvalho e Amburana",
+            cidade: "Indaiabira",
+            estado: "MG"
+        },
+         
+         {
+            produto: 90757945,
+            alcool: "38%",
+            tempo: "03 anos",
+            madeira: "Carvalho Europeu",
+            cidade: "Mirassol",
+            estado: "SP"
+        },
+         
+         {
+            produto: 90758368,
+            alcool: "38%",
+            tempo: "06 anos",
+            madeira: "Amburana",
+            cidade: "Mirassol",
+            estado: "SP"
+        },
+         
+         {
+            produto: 90759583,
+            alcool: "38%",
+            tempo: "06 anos",
+            madeira: "Jequitibá",
+            cidade: "Mirassol",
+            estado: "SP"
+        },
+         
+         
+         {
+            produto: 90756932,
+            alcool: "38%",
+            tempo: "12 anos",
+            madeira: "Carvalho",
+            cidade: "Mirassol",
+            estado: "SP"
+        },
+         
+         {
+            produto: 90759203,
+            alcool: "38%",
+            tempo: "10 anos",
+            madeira: "Amendoim e Carvalho Americano",
+            cidade: "Mirassol",
+            estado: "SP"
+        },
+         
+         {
+            produto: 87015660,
+            alcool: "42%",
+            tempo: "02 anos",
+            madeira: "Amburana",
+            cidade: "Buenópolis",
+            estado: "MG"
+        },
+         
+         {
+            produto: 90807679,
+            alcool: "48%",
+            tempo: "-",
+            madeira: "Inox",
+            cidade: "Jandaia do Sul",
+            estado: "PR"
+        },
+         
+         {
+            produto: 89527984,
+            alcool: "40%",
+            tempo: "08 anos",
+            madeira: "Carvalho",
+            cidade: "Jandaia do Sul",
+            estado: "PR"
+        },
+         
+         {
+            produto: 89521120,
+            alcool: "40%",
+            tempo: "04 anos",
+            madeira: "Carvalho",
+            cidade: "Jandaia do Sul",
+            estado: "PR"
+        },
+         
+         {
+            produto: 84288864,
+            alcool: "48%",
+            tempo: "01 ano",
+            madeira: "Amburana",
+            cidade: "Januária",
+            estado: "MG"
+        },
+         
+         {
+            produto: 90761641,
+            alcool: "38%",
+            tempo: "03 anos",
+            madeira: "Amburana, Bálsamo, Cabreúva, Angico, Grápia, Louro, Jequtibá-rosa ",
+            cidade: "Bento Gonçalves",
+            estado: "RS"
+        },
+         
+         {
+            produto: 90760235,
+            alcool: "38%",
+            tempo: "03 anos",
+            madeira: "Bálsamo",
+            cidade: "Bento Gonçalves",
+            estado: "RS"
+        },
+         
+         {
+            produto: 117427452,
+            alcool: "39%",
+            tempo: "-",
+            madeira: "Inox",
+            cidade: "Pirassununga",
+            estado: "SP"
+        },
+         
+         {
+            produto: 85795823,
+            alcool: "44%",
+            tempo: "04 anos",
+            madeira: "Bálsamo",
+            cidade: "Salinas",
+            estado: "MG"
+        },
+         
+         {
+            produto: 110264012,
+            alcool: "43%",
+            tempo: "01 ano",
+            madeira: "Cerejeira",
+            cidade: "São Rqoue do Canaã",
+            estado: "ES"
+        },
+         
+         
+         {
+            produto: 147144894,
+            alcool: "40%",
+            tempo: "01 ano",
+            madeira: "Amburana",
+            cidade: "Nova União",
+            estado: "MG"
+        },
+         
+         {
+            produto: 90758749,
+            alcool: "38%",
+            tempo: "15 anos",
+            madeira: "Carvalho",
+            cidade: "Mirassol",
+            estado: "SP"
+        },
+         
+         {
+            produto: 91943968,
+            alcool: "42%",
+            tempo: "-",
+            madeira: "Bálsamo",
+            cidade: "Salinas",
+            estado: "MG"
+        },
+         
+         {
+            produto: 84280501,
+            alcool: "42%",
+            tempo: "02 anos",
+            madeira: "Bálsamo",
+            cidade: "Salinas",
+            estado: "MG"
+        },
+         
+         {
+            produto: 148563250,
+            alcool: "40%",
+            tempo: "02 anos",
+            madeira: "Carvalho",
+            cidade: "Maquiné",
+            estado: "RS"
+        },
+         
+         {
+            produto: 148564648,
+            alcool: "40%",
+            tempo: "10 anos",
+            madeira: "Carvalho Europeu",
+            cidade: "Maquiné",
+            estado: "RS"
+        },
+         
+         {
+            produto: 148563986,
+            alcool: "40%",
+            tempo: "01 ano",
+            madeira: "Amburana",
+            cidade: "Maquiné",
+            estado: "RS"
+        },
+         
+         {
+            produto: 148569833,
+            alcool: "40%",
+            tempo: "03 anos",
+            madeira: "Carvalho",
+            cidade: "Pitangui",
+            estado: "MG"
+        },
+         
+         {
+            produto: 148569525,
+            alcool: "40%",
+            tempo: "03 anos",
+            madeira: "Amburana",
+            cidade: "Pitangui",
+            estado: "MG"
+        },
+         
+         {
+            produto: 148569010,
+            alcool: "39%",
+            tempo: "06 meses",
+            madeira: "Inox",
+            cidade: "Pitangui",
+            estado: "MG"
+        },
+         
+         {
+            produto: 148568688,
+            alcool: "39%",
+            tempo: "05 anos",
+            madeira: "Carvalho Francês e Bálsamo",
+            cidade: "Pitangui",
+            estado: "MG"
+        },
+         
+         {
+            produto: 148568230,
+            alcool: "39%",
+            tempo: "02 anos",
+            madeira: "Bálsamo",
+            cidade: "Pitangui",
+            estado: "MG"
+        },
+         
+         {
+            produto: 148566336,
+            alcool: "40%",
+            tempo: "01 ano e 06 meses",
+            madeira: "Amburana",
+            cidade: "Pitangui",
+            estado: "MG"
+        },
+         
+         {
+            produto: 148580165,
+            alcool: "40%",
+            tempo: "01 ano",
+            madeira: "Carvalho",
+            cidade: "Coronel Xavier Chaves",
+            estado: "MG"
+        },
+         
+         {
+            produto: 96444241,
+            alcool: "40%",
+            tempo: "03 anos",
+            madeira: "Bálsamo",
+            cidade: "Salinas",
+            estado: "MG"
+        },
+         
+         {
+            produto: 110256770,
+            alcool: "40%",
+            tempo: "02 anos",
+            madeira: "Bálsamo e Jequitibá",
+            cidade: "Salinas",
+            estado: "MG"
+        },
+         
+         {
+            produto: 110255702,
+            alcool: "40%",
+            tempo: "02 anos",
+            madeira: "Bálsamo e Jequitibá",
+            cidade: "Salinas",
+            estado: "MG"
+        },
+        
+        
+        {
+            produto: 84285497,
+            alcool: "47%",
+            tempo: "12 anos",
+            madeira: "Bálsamo",
+            cidade: "Salinas",
+            estado: "MG"
+        },
+        
+        {
+            produto: 144902213,
+            alcool: "45%",
+            tempo: "03 anos",
+            madeira: "Carvalho Americano de Primeiro Uso - Tostas 2, 3 e 4",
+            cidade: "Faria Lemos",
+            estado: "MG"
+        },
+        
+        {
+            produto: 144901001,
+            alcool: "40%",
+            tempo: "01 ano",
+            madeira: "Carvalho Americano 3AOB",
+            cidade: "Faria Lemos",
+            estado: "MG"
+        },
+        
+        {
+            produto: 144552895,
+            alcool: "21%",
+            tempo: "-",
+            madeira: "-",
+            cidade: "Belo Horizonte",
+            estado: "MG"
+        },
+        
+        {
+            produto: 147144221,
+            alcool: "16%",
+            tempo: "-",
+            madeira: "-",
+            cidade: "Caeté",
+            estado: "MG"
+        },
+        
+        
+        {
+            produto: 148580939,
+            alcool: "40%",
+            tempo: "2 anos",
+            madeira: "Carvalho e Amburana",
+            cidade: "Bom Jesus do Amparo",
+            estado: "MG"
+        },
+        
+        {
+            produto: 148581342,
+            alcool: "40%",
+            tempo: "2 anos",
+            madeira: "Amburana",
+            cidade: "Bom Jesus do Amparo",
+            estado: "MG"
+        },
+        
+        {
+            produto: 144038203,
+            alcool: "45,3%",
+            tempo: "14 anos",
+            madeira: "Bálsamo da Bahia",
+            cidade: "Salinas",
+            estado: "MG"
+        }
+        
+        
+        
+        
+    ]; // Fim de produtos detalhes
